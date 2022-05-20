f7191aaf3f4604877133018cff2f6c61994aaf9f
Daniil <flyjeray.dev@gmail.com>
2022-05-20 22:26:42 +0300

    SpecificPageToken: Finally Made a Chart
    Reverted changes because decided to use React-vis instead of Recharts

--- src/Components/SpecificTokenPage/SpecificTokenPage.tsx
+++ src/Components/SpecificTokenPage/SpecificTokenPage.tsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from "react";
+import { useMediaQuery } from "react-responsive";
+import { XYPlot, LineSeries, VerticalGridLines, HorizontalGridLines, XAxis, YAxis } from 'react-vis';
 
 import "./SpecificTokenPage.css"
 
@@ -21,8 +23,20 @@ interface FetchedData {
   timestamp: number
 }
 
+interface FetchedPriceData {
+  data: [{
+    priceUsd: string,
+    time: number,
+    date: string
+  }],
+  timestamp: number
+}
+
 function SpecificTokenPage() {
   const [FetchedData, SetFetchedData] = useState<FetchedData>();
+
+  const [ChartData, SetChartData] = useState<{x: number, y: number}[]>([]);
+  const [CurrentGraphWidth, SetCurrentGraphWidth] = useState<number>(900);
 
   const [TokenPrice, SetTokenPrice] = useState<string>('');
   const [MarketCap, SetMarketCap] = useState<string>('');
@@ -37,8 +51,51 @@ function SpecificTokenPage() {
   }, [FetchedData]);
 
   useEffect(() => {
+    if (isPhone.valueOf()) {
+      onPhoneSizeHandle(true);
+    }
+    else if (isTablet.valueOf()) {
+      onTabletSizeHandle(true);
+    }
+    else if (isDesktop.valueOf()) {
+      onDesktopSizeHandle(true);
+    }
+    
     PullTokenData();
   }, []);
+
+  const onPhoneSizeHandle = (matches: boolean) => {
+    if (matches) {
+      SetCurrentGraphWidth(300);
+      console.log('phone');
+    }
+  }
+
+  const onTabletSizeHandle = (matches: boolean) => {
+    if (matches) {
+      SetCurrentGraphWidth(600);
+      console.log('tablet');
+    }
+  }
+
+  const onDesktopSizeHandle = (matches: boolean) => {
+    if (matches) {
+      SetCurrentGraphWidth(900);
+      console.log('desktop');
+    }
+  }
+
+  const isPhone = useMediaQuery(
+    {maxWidth: 500}, undefined, onPhoneSizeHandle
+  )
+
+  const isTablet = useMediaQuery(
+    {minWidth:501, maxWidth: 950}, undefined, onTabletSizeHandle
+  )
+  
+  const isDesktop = useMediaQuery(
+    {minWidth: 951}, undefined, onDesktopSizeHandle
+  )
 
   const ConvertTokenPrice = () => {
     let NewTokenPrice: string;
@@ -85,6 +142,32 @@ function SpecificTokenPage() {
 
     SetMarketCap(NewMarketCap);
   }
+
+  const PullChartData = async (tokenName: string) => {
+    const endDate = new Date();
+
+    const startDate = new Date(endDate);
+    startDate.setDate(startDate.getDate() - 7);
+
+    const data = await fetch(`https://api.coincap.io/v2/assets/${tokenName}/history?start=${startDate.getTime()}&end=${endDate.getTime()}&interval=d1`);
+    const json: FetchedPriceData = await data.json();
+
+    const newChartData: {x: number, y: number}[] = [];
+
+    json.data.forEach((dataBlock) => {
+      const y: number = parseFloat(parseFloat(dataBlock.priceUsd).toFixed(2));
+
+      const date: Date = new Date(dataBlock.time);
+      const x: number = date.getDate();
+
+      const chartBlock: {x: number, y: number} = {x, y};
+
+      newChartData.push(chartBlock);
+    });
+
+    console.log(newChartData);
+    SetChartData(newChartData);
+  }
  
   const PullTokenData = async () => {
     // Getting token name from URL.
@@ -95,9 +178,10 @@ function SpecificTokenPage() {
     const token = url.split('_')[1];
 
     const data = await fetch(`https://api.coincap.io/v2/assets/${token}`);
-    const json = await data.json();
+    const json: FetchedData = await data.json();
 
     SetFetchedData(json);
+    PullChartData(json.data.id);
   }
 
   return(
@@ -109,6 +193,23 @@ function SpecificTokenPage() {
           <p style={{'color': PercentChangeColor}}>{PercentChange}</p>
         </div>
         <p>{MarketCap}</p>
+        <XYPlot style={{'fontFamily': 'Kanit', fill: 'none', 'fontSize': 8}} height={300} width={CurrentGraphWidth}>
+          <LineSeries 
+            color={'white'}
+            data={ChartData} />
+          <VerticalGridLines style={{'stroke': 'gray'}}/>
+          <HorizontalGridLines style={{'stroke': 'gray'}}/>
+          <XAxis 
+            style={{
+              line: {stroke: '#ADDDE1'},
+              ticks: {stroke: '#ADDDE1'},
+            }} />
+          <YAxis
+            style={{
+              line: {stroke: '#ADDDE1'},
+              ticks: {stroke: '#ADDDE1'}
+            }} />
+        </XYPlot>
       </div>
     </div>
   )
